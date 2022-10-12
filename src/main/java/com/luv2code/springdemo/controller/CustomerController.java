5254d7c4e454490787a2959fdb89a28766b71a0d
Tailoc <taitranhuu08@gmail.com>
2022-10-12 23:18:55 +0700

    commit one

---
+++ src/main/java/com/luv2code/springdemo/controller/CustomerController.java
@@ -0,0 +1,87 @@
+package com.luv2code.springdemo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.luv2code.springdemo.entity.Customer;
+import com.luv2code.springdemo.service.CustomerService;
+
+@Controller
+@RequestMapping("/customer")
+public class CustomerController {
+	
+	// need to inject the customer dao
+	@Autowired
+	private CustomerService customerService;
+
+	@GetMapping("/list")
+	public String listCustomers(Model model) {
+		
+		// get customers from the dao
+		List<Customer> theCustomers = customerService.getCustomers();
+		
+		System.out.println();
+		// add the customers to the model
+		model.addAttribute("customers", theCustomers);
+		
+		return "list-customers";
+	}
+	
+	@GetMapping("/showFormForAdd")
+	public String showFormForAdd(Model model) {
+		
+		Customer theCustomer = new Customer();
+		
+		model.addAttribute("customer", theCustomer);
+		
+		return "customer-form";
+	}
+	
+	@PostMapping("/saveCustomer")
+	public String saveCustomer(@ModelAttribute("customer") Customer theCustomer) {
+		
+		customerService.saveCustomer(theCustomer);
+		
+		return "redirect:/customer/list";
+	}
+	
+	@GetMapping("/showFormForUpdate")
+	public String showFormForUpdate(@RequestParam("customerId") int theId,
+								Model model) {
+		
+		Customer theCustomer = customerService.getCustomer(theId);
+		
+		model.addAttribute("customer", theCustomer);
+		
+		return "customer-form";
+	}
+	
+	@GetMapping("/delete")
+	public String deleteCustomer(@RequestParam("customerId") int theId) {
+		
+		customerService.deleteCustomer(theId);
+		
+		return "redirect:/customer/list";
+	}
+	
+	@GetMapping("/search")
+    public String searchCustomers(@RequestParam("theSearchName") String theSearchName,
+                                    Model theModel) {
+
+        // search customers from the service
+        List<Customer> theCustomers = customerService.searchCustomers(theSearchName);
+                
+        // add the customers to the model
+        theModel.addAttribute("customers", theCustomers);
+
+        return "list-customers";        
+    }
+}
